11045dcb1b4d1181ee3a25248fe74a73e51e1f08
Rajiv Seelam <rajiv@betalectic.com>
2024-10-11 11:13:32 +0530

    Add correct reponame to config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,9 +20,10 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "cybrilla", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
 
+  trailingSlash: false,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
