d72217d1b3ee6ad8e8564b57dbfb630c68cbb861
Rajiv Seelam <rajiv@betalectic.com>
2024-10-11 11:05:30 +0530

    Initial Commit + Actions

---
+++ docusaurus.config.ts
@@ -0,0 +1,149 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const BASE_URL =
+  process.env.PR_NUMBER !== undefined
+    ? `/docs/pr-preview/pr-${process.env.PR_NUMBER}/`
+    : "/docs/";
+
+const config: Config = {
+  title: "Cybrilla RTA",
+  tagline: "Dinosaurs are cool",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://cybrilla.github.io",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: BASE_URL,
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+        },
+        blog: false,
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "",
+      logo: {
+        alt: "Cybrilla logo",
+        src: "img/cybrilla-logo-full.webp",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "sidebar",
+          position: "left",
+          label: "Docs",
+        },
+        {
+          href: "https://fintechprimitives.com/api/",
+          label: "APIs",
+          position: "right",
+        },
+        {
+          type: "dropdown",
+          label: "About us",
+          position: "right",
+          items: [
+            {
+              label: "Fintech primitives",
+              href: "https://fintechprimitives.com/",
+            },
+            {
+              label: "Cybrilla technologies",
+              href: "https://www.cybrilla.com/",
+            },
+            {
+              label: "Careers",
+              href: "https://www.cybrilla.com/careers.html",
+            },
+          ],
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Docs",
+              to: "/docs/introduction",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Cybrilla Technologies`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+    docs: {
+      sidebar: {
+        hideable: true,
+      },
+    },
+  } satisfies Preset.ThemeConfig,
+
+  plugins: ["docusaurus-lunr-search"],
+};
+
+export default config;
